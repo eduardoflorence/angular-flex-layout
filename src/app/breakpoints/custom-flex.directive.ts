52fd3ed826bd4336d9f89825e3504013b3fe6590
Eduardo Florence <eduardo@geminisistemas.com>
2019-04-16 12:00:12 -0300

    Fechamento da versão 1.0

--- src/app/breakpoints/custom-flex.directive.ts
+++ src/app/breakpoints/custom-flex.directive.ts
@@ -1,12 +1,12 @@
 import { Directive } from '@angular/core';
 import { FlexDirective } from '@angular/flex-layout';
 
-const selector = `[fxFlex.smd],
-                  [fxFlex.lt-smd],
-                  [fxFlex.gt-smd]`;
-const inputs = ['fxFlex.smd',
-                'fxFlex.lt-smd',
-                'fxFlex.gt-smd'
+const selector = '[fxFlex.smd], [fxFlex.lt-smd], [fxFlex.gt-smd]';
+
+const inputs = [
+  'fxFlex.smd',
+  'fxFlex.lt-smd',
+  'fxFlex.gt-smd'
 ];
 
 // tslint:disable-next-line:use-input-property-decorator
--- src/app/breakpoints/custom-show-hide.directive.ts
+++ src/app/breakpoints/custom-show-hide.directive.ts
@@ -1,19 +1,15 @@
 import { Directive } from '@angular/core';
 import { ShowHideDirective } from '@angular/flex-layout';
 
-const selector = `[fxHide.smd],
-                  [fxHide.lt-smd],
-                  [fxHide.gt-smd],
-                  [fxShow.smd],
-                  [fxShow.lt-smd],
-                  [fxShow.gt-smd]`;
+const selector = '[fxHide.smd], [fxHide.lt-smd], [fxHide.gt-smd], [fxShow.smd], [fxShow.lt-smd], [fxShow.gt-smd]';
 
-const inputs = ['fxHide.smd',
-                'fxHide.lt-smd',
-                'fxHide.gt-smd',
-                'fxShow.smd',
-                'fxShow.lt-smd',
-                'fxShow.gt-smd'
+const inputs = [
+  'fxHide.smd',
+  'fxHide.lt-smd',
+  'fxHide.gt-smd',
+  'fxShow.smd',
+  'fxShow.lt-smd',
+  'fxShow.gt-smd'
 ];
 
 // tslint:disable-next-line:use-input-property-decorator
